b35f6fd4e4d6e2105d8c50d271124fd11271abf0
zhzh5724 <zhzh5724@gmail.com>
2017-12-19 22:30:40 +0800

    定义项目根目录

--- bin/zz-init.js
+++ bin/zz-init.js
@@ -1,8 +1,11 @@
 #!/user/bin/env node
 
 const program = require('commander')
+const path = require('path')
+const fs = require('fs')
+const glob = require('glob')
 
-program.usage('<project-name>').parse(process.argv)
+program.usage('<project-name>')
 
 let projectName = program.args[0]
 
@@ -11,8 +14,26 @@ if(!projectName){
     return
 }
 
+const list = glob.sync('*')
+let rootName = path.basename(process.cwd())
+if(list.length){
+    if(list.filter(name => {
+        const fileName = path.resolve(process.cwd(),path.join('.',name))
+        const isDir = fs.stat(fileName).isDirectory()
+        return name.indexOf(projectName) !== -1 && isDir
+    }).length !== 0){
+        console.log(`项目${projectName}已经存在`)
+        return
+    }
+    rootName = projectName
+}else if(rootName === projectName){
+    rootName = '.'
+}else{
+    rootName = projectName
+}
+
 go()
 
 function go () {
-    
+    console.log(path.resolve(process.cwd(),path.join('.',rootName)))
 }
