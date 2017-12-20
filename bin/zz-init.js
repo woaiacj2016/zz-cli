5729601814f595118ea8cb3efa6cdb5c8532f09d
zhzh5724 <zhzh5724@gmail.com>
2017-12-20 22:36:51 +0800

    下载模板功能

--- bin/zz-init.js
+++ bin/zz-init.js
@@ -5,7 +5,7 @@ const path = require('path')
 const fs = require('fs')
 const glob = require('glob')
 
-program.usage('<project-name>')
+program.usage('<project-name>').parse(process.argv)
 
 let projectName = program.args[0]
 
@@ -19,7 +19,7 @@ let rootName = path.basename(process.cwd())
 if(list.length){
     if(list.filter(name => {
         const fileName = path.resolve(process.cwd(),path.join('.',name))
-        const isDir = fs.stat(fileName).isDirectory()
+        const isDir = fs.statSync(fileName).isDirectory()
         return name.indexOf(projectName) !== -1 && isDir
     }).length !== 0){
         console.log(`项目${projectName}已经存在`)
